fbba0a9876c147809676ff630cb2b9fdb423b649
Douglas Eduardo Rosa <douglas.erosa@gmail.com>
2016-06-15 01:44:45 -0300

    - benchmark IS em c++ (problema no makefile).

---
+++ is/cpp/IS/npbparams.h
@@ -0,0 +1,14 @@
+#define CLASS 'A'
+/*
+   This file is generated automatically by the setparams utility.
+   It sets the number of processors and the class of the NPB
+   in this directory. Do not modify it by hand.   */
+   
+#define COMPILETIME "15 Jun 2016"
+#define NPBVERSION "3.3.1"
+#define CC "g++"
+#define CFLAGS "-g -Wall -O3 -lpthread -std=c++11"
+#define CLINK "$(CC)"
+#define CLINKFLAGS "-O3 -lpthread -std=c++11"
+#define C_LIB "-lpthread -lm -std=c++11"
+#define C_INC "-I../common"
