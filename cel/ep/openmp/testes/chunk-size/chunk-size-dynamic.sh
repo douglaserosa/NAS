54c1e28e89f20f34a403dcbaca23dd86c59d98f5
Douglas Eduardo Rosa <douglas@Douglas-MacBook-Pro.local>
2018-07-05 02:38:29 -0300

    testes de tamanho de chunk para cada tipo de schedule

---
+++ cel/ep/openmp/testes/chunk-size/chunk-size-dynamic.sh
@@ -0,0 +1,25 @@
+#!/system/bin/sh
+
+# ----- EP-OPENMP-DYNAMIC
+for class in S
+do
+	for threads in 4
+	do
+        for chunk in {1,2,4,8,16,32,64,128,256,512,1024,2048,4096,8192,16384,32768,65536,131072,262144,524288,1048576,2097152,4194304,8388608,16777216,33554432}
+        do
+            for i in {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30}
+            do
+                echo "Running: ep-openmp-dynamic"
+                echo "Class  :" $class
+                echo "Threads:" $threads 
+                echo "Chunk  :" $chunk
+                echo "Exec   :" $i
+                echo ""
+                ./ep-openmp-dynamic $class $threads $chunk > results/chunk-size-test/dynamic/ep-openmp-dynamic-$class-$threads-$chunk--$i
+                ./battery >> results/chunk-size-test/dynamic/ep-openmp-dynamic-$class-$threads-$chunk--$i
+            done
+        done
+	done
+done
+
+echo "DONE"
