29818638f2183b84e68d9dcbce1155fd76ef192d
Douglas Eduardo Rosa <douglas@Douglas-MacBook-Pro.local>
2018-07-08 02:31:55 -0300

    resultado teste chunk-size. scripts para teste de eficiencia openmp

--- cel/ep/openmp/testes/eficiencia/eficiencia-parallel.sh
+++ cel/ep/openmp/testes/eficiencia/eficiencia-parallel.sh
@@ -1,26 +1,33 @@
 #!/system/bin/sh
 
 # ----- EP-OPENMP
-data=$(date +"%Y%m%d%H%M%S")
-for class in {S,W}
+for test in {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30}
 do
-	for threads in {2,4}
-	do
-        for chunk in {1024,1048576}
+    sleep 10
+    data=$(date +"%Y%m%d%H%M%S")
+    mkdir results/eficiencia/parallel/$data
+    chmod 777 -R results/eficiencia/parallel/$data
+    for class in {S,W}
+    do
+        for threads in {2,4}
         do
-            for i in {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30}
+            for chunk in {16384,1048576}
             do
-                echo "Running: ep-openmp"
-                echo "Class  :" $class
-                echo "Threads:" $threads 
-                echo "Chunk  :" $chunk
-                echo "Exec   :" $i
-                echo ""
-                ./ep-openmp $class $threads $chunk > results/eficiencia/parallel/$data/ep-openmp-$class-$threads-$chunk--$i
-                ./battery >> results/eficiencia/parallel/$data/ep-openmp-$class-$threads-$chunk--$i
+                for i in {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30}
+                do
+                    echo "Running: ep-openmp"
+                    echo "Class  :" $class
+                    echo "Threads:" $threads
+                    echo "Chunk  :" $chunk
+                    echo "Exec   :" $i
+                    echo "Test   :" $test
+                    echo ""
+                    ./ep-openmp $class $threads $chunk > results/eficiencia/parallel/$data/ep-openmp-$class-$threads-$chunk--$i
+                    ./battery >> results/eficiencia/parallel/$data/ep-openmp-$class-$threads-$chunk--$i
+                done
             done
         done
-	done
+    done
 done
 
 echo "DONE"
